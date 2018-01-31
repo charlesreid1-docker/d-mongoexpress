4e8acc34377cdb67620d5514b535692526a0fe1a
Charles Reid <charlesreid1@gmail.com>
2018-01-30 23:29:36 -0800

    still not sure what we changed, but things are working okay now.

--- run_stock.sh
+++ run_stock.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
+#
+# don't touch the --link line.
 
 docker run \
-	-it --rm \
-	-p 8081:8081 \
-	--link inspiring_boyd:mongo \
-	-e ME_CONFIG_OPTIONS_EDITORTHEME="ambiance" \
-	-e ME_CONFIG_MONGODB_ENABLE_ADMIN="true" \
-	-e ME_CONFIG_MONGODB_ADMINUSERNAME="mongo" \
-	-e ME_CONFIG_MONGODB_ADMINPASSWORD="dummy" \
-	-e ME_CONFIG_BASICAUTH_USERNAME="wifi" \
-	-e ME_CONFIG_BASICAUTH_PASSWORD="dummy" \
-	-d \
-	mongo-express
+    -it \
+    --rm \
+    --link happy_mongo:mongo \
+    -p 8081:8081 \
+    -e ME_CONFIG_OPTIONS_EDITORTHEME="ambiance" \
+    -e ME_CONFIG_MONGODB_ENABLE_ADMIN="true" \
+    -e ME_CONFIG_MONGODB_ADMINUSERNAME="mongo" \
+    -e ME_CONFIG_MONGODB_ADMINPASSWORD="dummy" \
+    -e ME_CONFIG_BASICAUTH_USERNAME="wifi" \
+    -e ME_CONFIG_BASICAUTH_PASSWORD="dummy" \
+    -d \
+    mongo-express
 
