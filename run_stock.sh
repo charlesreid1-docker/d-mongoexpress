a63f748dc04ae7bad3efcb762fe86b9ea9a4d47f
Charles Reid <charlesreid1@gmail.com>
2018-02-03 16:53:25 -0800

    update with latest working stuff.

--- run_stock.sh
+++ run_stock.sh
@@ -2,17 +2,37 @@
 #
 # don't touch the --link line.
 
-docker run \
-    -it \
-    --rm \
-    --link happy_mongo:mongo \
-    -p 8081:8081 \
-    -e ME_CONFIG_OPTIONS_EDITORTHEME="ambiance" \
-    -e ME_CONFIG_MONGODB_ENABLE_ADMIN="true" \
-    -e ME_CONFIG_MONGODB_ADMINUSERNAME="mongo" \
-    -e ME_CONFIG_MONGODB_ADMINPASSWORD="dummy" \
-    -e ME_CONFIG_BASICAUTH_USERNAME="wifi" \
-    -e ME_CONFIG_BASICAUTH_PASSWORD="dummy" \
-    -d \
-    mongo-express
+debug=false
+#debug=true
+
+docker ps -qa | xargs docker rm
+
+if [ "$debug" == true ]; then
+
+    docker run \
+        --name mex \
+        -p 8081:8081 \
+        --link happy_mongo:mongo \
+        -e ME_CONFIG_OPTIONS_EDITORTHEME="ambiance" \
+        -e ME_CONFIG_BASICAUTH_USERNAME="wifi" \
+        -e ME_CONFIG_BASICAUTH_PASSWORD="dummy" \
+        -ti mongo-express \
+        /bin/bash
+
+        #-e ME_CONFIG_MONGODB_URL="10.6.0.1" \
+        #-e ME_CONFIG_MONGODB_PORT="27017" \
+
+else
+
+    docker run \
+        --name mex \
+        -p 8081:8081 \
+        --link happy_mongo:mongo \
+        -e ME_CONFIG_OPTIONS_EDITORTHEME="ambiance" \
+        -e ME_CONFIG_BASICAUTH_USERNAME="wifi" \
+        -e ME_CONFIG_BASICAUTH_PASSWORD="dummy" \
+        -d \
+        -ti mongo-express 
+
+fi
 
